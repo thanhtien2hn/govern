50a0949526225a4041cf6f68dc5af212b448c601
thanhtien2hn <thanhtien0123456789@gmail.com>
2023-12-14 21:17:47 +0700

    commit

---
+++ src/Code/Dashboard.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function Dashboard() {
+  return (
+    <button>OKOK</button>
+  );
+}
+
+export default Dashboard;
